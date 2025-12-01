ef48e1bcc03bb8788c9bbdc30384aac59061460e
SameerKhurram <sp23-bse-003@cuiatk.edu.pk>
2025-12-01 20:01:53 +0500

    Updated App.js after Group Members Screens

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import HomeScreen from "./screens/HomeScreen";
+import AddExpenseScreen from "./screens/AddExpenseScreen";
+import ExpenseHistoryScreen from "./screens/ExpenseHistoryScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,28 +15,41 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-
-        {/* Login Screen */}
         <Stack.Screen
           name="Login"
           component={LoginScreen}
           options={{ headerShown: false }}
         />
 
-        {/* Signup Screen */}
         <Stack.Screen
           name="Signup"
           component={SignupScreen}
           options={{ headerShown: false }}
         />
 
-        {/* Home Screen */}
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ headerShown: false }}
         />
 
+        <Stack.Screen
+          name="AddExpense"
+          component={AddExpenseScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="ExpenseHistory"
+          component={ExpenseHistoryScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
